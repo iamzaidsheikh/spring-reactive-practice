113ebca532af1abf152e22b2fec8fe6556188646
zaid <iamzaidsheikh@gmail.com>
2022-01-21 22:50:26 +0530

    Initial commit

---
+++ spring-reactive-practice/src/test/java/io/github/iamzaidsheikh/springreactivepractice/SpringReactivePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.iamzaidsheikh.springreactivepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactivePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
